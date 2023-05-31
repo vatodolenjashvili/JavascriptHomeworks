af73b15107fd8bdf5f82e78b1e9595b1ea0e510e
vatodolenjashvili <126957782+vatodolenjashvili@users.noreply.github.com>
2023-05-31 16:07:31 +0400

    Add files via upload

---
+++ JavascriptHomework 1/main.js
@@ -0,0 +1,45 @@
+/*
+Problem 1:
+
+შექმენით შემდეგი ცვლადები და მიანიჭეთ მათ თქვენთვის სასურველი მნიშვნელობები:
+name
+lastName
+birthYear
+gender
+*/
+
+let firstName = "vato";
+let lastName = "dolenjashvili";
+let birthYear = 2004;
+let gender = "male";
+
+console.log(firstName);
+console.log(lastName);
+console.log(birthYear);
+console.log(gender);
+/*
+Problem 2:
+
+How many expressions are in the following statement?
+*/
+
+100 - ((10 / 2) * 7) / 5;
+
+4
+
+/*
+Problem 3:
+
+შექმენით 2 ცვალდი (country, population).
+country ცვლადს მიანიჭეთ ნებისმიერი ქვეყნის სახელი, დაბალ რეგისტრში (lower case)
+population ცვლადს მიანითჭეთ ქვეყნის შესაბამისი მაცხოვრებლების რაოდენობა მილიონებში (მაგ. 3.5 და არა 3500000)
+გამოიტანეთ ეს მიშნველობები შემდეგი სახით
+„United Kingdom’s population is 67.33 million“
+
+*/
+
+let country = "georgia";
+
+let population = "3.5";
+
+console.log(country + "'s population is " + population + " million");
